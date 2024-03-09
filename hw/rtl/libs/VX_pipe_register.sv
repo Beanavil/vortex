4ff5db5edf4b62f98f347a84219343c1b1cd1644
Beatriz Navidad Vilches <beatriz@streamhpc.com>
2024-03-09 17:54:29 +0100

    Trim end-line whitespaces

--- hw/rtl/libs/VX_pipe_register.sv
+++ hw/rtl/libs/VX_pipe_register.sv
@@ -1,10 +1,10 @@
-// Copyright © 2019-2023
-// 
+// Copyright © 2019-2024
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,9 +14,9 @@
 `include "VX_platform.vh"
 
 `TRACING_OFF
-module VX_pipe_register #( 
-    parameter DATAW  = 1, 
-    parameter RESETW = 0, 
+module VX_pipe_register #(
+    parameter DATAW  = 1,
+    parameter RESETW = 0,
     parameter DEPTH  = 1
 ) (
     input wire              clk,
@@ -25,12 +25,12 @@ module VX_pipe_register #(
     input wire [DATAW-1:0]  data_in,
     output wire [DATAW-1:0] data_out
 );
-    if (DEPTH == 0) begin        
+    if (DEPTH == 0) begin
         `UNUSED_VAR (clk)
         `UNUSED_VAR (reset)
         `UNUSED_VAR (enable)
-        assign data_out = data_in;  
-    end else if (DEPTH == 1) begin        
+        assign data_out = data_in;
+    end else if (DEPTH == 1) begin
         if (RESETW == 0) begin
             `UNUSED_VAR (reset)
             reg [DATAW-1:0] value;
@@ -68,11 +68,11 @@ module VX_pipe_register #(
                 if (enable) begin
                     value_d <= data_in[DATAW-RESETW-1:0];
                 end
-            end        
+            end
             assign data_out = {value_r, value_d};
         end
     end else begin
-        wire [DEPTH:0][DATAW-1:0] data_delayed;        
+        wire [DEPTH:0][DATAW-1:0] data_delayed;
         assign data_delayed[0] = data_in;
         for (genvar i = 1; i <= DEPTH; ++i) begin
             VX_pipe_register #(
